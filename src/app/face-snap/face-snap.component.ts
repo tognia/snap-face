3fd466d4e352164bfb11711a1567e5d194d1df22
tognia <h.tognia@ziyouma-agency.com>
2024-11-16 14:42:27 +0100

    String Interpolation Initiation

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent {
+  title! : string;
+  description! : string;
+  createdAt! : Date;
+  snaps! : number;
+
+  ngOnInit(): void{
+    this.title = "Archibald";
+    this.description = "Mon meilleur ami depuis toujours !";
+    this.createdAt = new Date();
+    this.snaps = 5;
+  }
+
+}
