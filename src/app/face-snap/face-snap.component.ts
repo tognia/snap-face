efe29c9038778ecb00650e3c439f2fb737cb8517
tognia <h.tognia@ziyouma-agency.com>
2024-11-16 14:59:19 +0100

    attribute binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,12 +12,14 @@ export class FaceSnapComponent {
   description! : string;
   createdAt! : Date;
   snaps! : number;
+  imageUrl! : string;
 
   ngOnInit(): void{
     this.title = "Archibald";
     this.description = "Mon meilleur ami depuis toujours !";
     this.createdAt = new Date();
     this.snaps = 5;
+    this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
   }
 
 }
