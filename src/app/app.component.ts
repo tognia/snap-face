885fba73dbbd41e952eccfbf8f2caedd3267ff41
tognia <h.tognia@ziyouma-agency.com>
2024-12-14 11:48:31 +0100

    Apply styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
         'Mon meilleur ami depuis toujours !',
         'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
         new Date(),
-        10
+        1000
       ),
       new FaceSnap(
         'Three Rock Mountain',
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { NgStyle, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle, NgClass
+  ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
