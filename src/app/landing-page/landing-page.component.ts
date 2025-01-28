ad495cf6d2b524ca7d6aa1ff29a2023e3dd8994b
tognia <h.tognia@ziyouma-agency.com>
2025-01-28 10:30:49 +0100

    Add RouterLink

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  onContinue(): void {
+
+  }
 
 }
